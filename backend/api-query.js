8e62dd08a00f14402c188eb63246da015429c493
ParkJeeHoon <gkstj8300@naver.com>
2022-11-30 17:06:22 +0900

    query 진행중

--- backend/api-query.js
+++ backend/api-query.js
@@ -29,28 +29,3 @@ app.put("/api/memos/:i", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-app.use(bodyParser.json());
-
-app.get("/api/memos", async (req, res) => {
-  const result = await database.run("SELECT * FROM MEMOS;");
-  res.send(result);
-});
-
-app.post("/api/memos", async (req, res) => {
-  await database.run(
-    `INSERT INTO memos (content) VALUES ('${req.body.content}')`
-  );
-  const result = await database.run("SELECT * FROM MEMOS;");
-  // memos.push(req.body.content);
-  res.send(result);
-});
-
-app.put("/api/memos/:i", (req, res) => {
-  memos[req.params.i] = req.body.content;
-  res.send(memos);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
